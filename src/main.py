dd7a1098f096c1ed2521d7033d20f2f498948f0f
Altamis07 <altamisahamed2008@gmail.com>
2026-07-11 20:26:19 +0530

    Added Frame extraction
    extract frames every 5 seconds

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-import cv2
-from pathlib import  Path
-import config
-from config import SAMPLE_VIDEOS_DIR
+from logging import currentframe
 
-video_path = SAMPLE_VIDEOS_DIR/"test.mp4"
+import cv2
+import config
+
+#Load videos
+video_path = config.SAMPLE_VIDEOS_DIR / "test.mp4" #name of video file in string
 cap = cv2.VideoCapture(str(video_path))
 
 if not cap.isOpened():
@@ -18,3 +19,26 @@ print("✅ Video Loaded")
 print(f"FPS: {fps}")
 print(f"Frames: {frame_count}")
 print(f"Duration: {duration:.2f} seconds")
+
+#Extract Frames
+
+saved_frames = 0
+current_frame = 0
+
+frame_interval = int(fps * 0.5)
+
+while True:
+    success, frame = cap.read()
+    if not success:
+        break
+
+    if current_frame % frame_interval == 0:
+        frames_dir = config.FRAMES_DIR/f"frame_{current_frame:04d}.jpg" # directory of saving and name the file
+        cv2.imwrite(frames_dir, frame)# save current working frame
+        saved_frames += 1
+
+    current_frame += 1
+
+cap.release()
+
+print(f"Total saved frames: {saved_frames}")
