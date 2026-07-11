dcbba6b9a2a775216bc584821d0ddd83d29a2636
Altamis07 <altamisahamed2008@gmail.com>
2026-07-11 19:50:54 +0530

    Added video loading feature
    added config to configure directories

---
+++ src/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+
+SAMPLE_VIDEOS_DIR = ROOT_DIR / "sample_vids"
+OUTPUT_DIR = ROOT_DIR / "output"
+FRAMES_DIR = OUTPUT_DIR / "frames"
+PDF_DIR = OUTPUT_DIR / "pdfs"
--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
 import cv2
+from pathlib import  Path
+import config
+from config import SAMPLE_VIDEOS_DIR
+
+video_path = SAMPLE_VIDEOS_DIR/"test.mp4"
+cap = cv2.VideoCapture(str(video_path))
+
+if not cap.isOpened():
+    print("Could not open video file")
+    exit(0)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+duration = frame_count / fps
+
+print("✅ Video Loaded")
+print(f"FPS: {fps}")
+print(f"Frames: {frame_count}")
+print(f"Duration: {duration:.2f} seconds")
